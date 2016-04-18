b04a7d7107a071e9f7a81b4d4a31f12a3b62186a
victorh3 <victorh3@live.com>
2016-04-18 00:40:25 -0400

    insert pushes to db

---
+++ imports/api/mongo.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const PublicProfileDB = new Mongo.Collection('publicProfile');
--- imports/ui/dashboard.js
+++ imports/ui/dashboard.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { PublicProfileDB } from '../api/mongo.js';
 
 import './dashboard.html';
 
@@ -16,10 +17,25 @@ Template.dashboard.events({
     event.preventDefault();
 
     var firstName = document.getElementById('firstName').value,
+    middleName = document.getElementById('middleName').value,
     lastName = document.getElementById('lastName').value,
-    phoneNumber = document.getElementById('phoneNumber1').value,
-    address = document.getElementById('address1').value,
+    nickName = document.getElementById('nickName').value,
+    phoneNumber1 = document.getElementById('phoneNumber1').value,
+    phoneNumber2 = document.getElementById('phoneNumber2').value,
+    address1 = document.getElementById('address1').value,
+    address2 = document.getElementById('address2').value,
     zipCode =  document.getElementById('zipCode').value,
-    cityState = document.getElementById('cityState').value;
+    cityState = document.getElementById('cityState').value,
+    bloodType = document.getElementById('bloodType').value,
+    nickName = document.getElementById('nickName').value,
+    weight = document.getElementById('weight').value,
+    dob = document.getElementById('dob').value;
+
+    // PublicProfileDB.insert({
+    //   name: firstName,
+    // });
   },
 });
+
+// var fetch = PublicProfileDB.find().fetch();
+// console.log(fetch);
