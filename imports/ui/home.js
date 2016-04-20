5f240b695bab1db0261c62c9f94004a1060c198b
victorh3 <victorh3@live.com>
2016-04-20 00:22:19 -0400

    spelling fix and feedback at login and register pages

--- imports/ui/home.js
+++ imports/ui/home.js
@@ -35,8 +35,3 @@ Template.home.events({
 
   }
 });
-
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     console.log("DOM fully loaded and parsed");
-//     document.getElementById('a').innerHTML = "Hello";
-//   });
